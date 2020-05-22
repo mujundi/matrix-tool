8fb97fb8e2597bc87d4c05bf05b6f893ffeb1b87
Musa Jundi <musajundi@gmail.com>
2020-05-22 14:40:01 -0500

    Added determinants, inverses, some formatting, and transposition.

--- Numeric Matrix Processor/task/src/processor/Matrix.java
+++ Numeric Matrix Processor/task/src/processor/Matrix.java
@@ -1,16 +1,18 @@
 package processor;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Arrays;
 
 public class Matrix {
     private final int numOfRows;
     private final int numOfColumns;
-    private int[][] matrix;
+    private double[][] matrix;
 
     public Matrix(int n, int m) {
         this.numOfRows = n;
         this.numOfColumns = m;
-        this.matrix = new int[n][m];
+        this.matrix = new double[n][m];
     }
 
     public int getNumOfRows() {
@@ -21,15 +23,15 @@ public class Matrix {
         return this.numOfColumns;
     }
 
-    public int[][] getMatrix() {
+    public double[][] getMatrix() {
         return this.matrix;
     }
 
-    public int getElement(int i, int j) {
+    public double getElement(int i, int j) {
         return this.matrix[i][j];
     }
 
-    public void setElement(int i, int j, int value) {
+    public void setElement(int i, int j, double value) {
         try {
             this.matrix[i][j] = value;
         } catch (Exception e) {
@@ -37,8 +39,8 @@ public class Matrix {
         }
     }
 
-    public void setMatrix(int[][] array) {
-        this.matrix = Arrays.stream(array).map(int[]::clone).toArray(int[][]::new);
+    public void setMatrix(double[][] array) {
+        this.matrix = Arrays.stream(array).map(double[]::clone).toArray(double[][]::new);
     }
 
     public boolean hasSameDimensionsOf(Matrix comparison) {
@@ -52,7 +54,7 @@ public class Matrix {
         } else {
             Matrix sum = new Matrix(this.getNumOfRows(), this.getNumOfColumns());
 
-            int[][] arrayToAdd = addend.getMatrix();
+            double[][] arrayToAdd = addend.getMatrix();
 
             for (int i = 0; i < this.getNumOfRows(); i++) {
                 for (int j = 0; j < this.getNumOfColumns(); j++) {
@@ -62,8 +64,180 @@ public class Matrix {
 
             return sum;
         }
-
     }
+
+    public Matrix multiplyBy(double factor) {
+        Matrix product = new Matrix(this.numOfRows, this.numOfColumns);
+        for (int i = 0; i < this.numOfRows; i++) {
+            for (int j = 0; j < this.numOfColumns; j++) {
+                product.setElement(i, j, this.matrix[i][j] * factor);
+            }
+        }
+
+        return product;
+    }
+
+    public Matrix multiplyBy(Matrix matrixTwo) {
+        if (this.numOfColumns != matrixTwo.getNumOfRows()) {
+            return null;
+        }
+        
+        Matrix product = new Matrix(this.getNumOfRows(), matrixTwo.getNumOfColumns());
+        double sum = 0;
+        for (int i = 0; i < product.getNumOfRows(); i++) {
+            for (int j = 0; j < product.getNumOfColumns(); j++) {
+                sum = 0;
+                for (int k = 0; k < this.numOfColumns; k++) {
+                    sum += this.getElement(i, k) * matrixTwo.getElement(k, j);
+                }
+                product.setElement(i, j, sum);
+            }
+        }
+        
+        return product;
+    }
+
+    public Matrix transposeByMain() {
+        int m = this.numOfRows;
+        int n = this.numOfColumns;
+        Matrix transposed = new Matrix(n, m);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transposed.setElement(i, j, this.matrix[j][i]);
+            }
+        }
+
+        return transposed;
+    }
+
+    public Matrix transposeBySide() {
+        int m = this.numOfRows;
+        int n = this.numOfColumns;
+        Matrix transposed = new Matrix(n, m);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transposed.setElement(i, j, this.matrix[m-j-1][n-i-1]);
+            }
+        }
+
+        return transposed;
+    }
+
+    public Matrix transposeByVertical() {
+        int m = this.numOfRows;
+        int n = this.numOfColumns;
+        Matrix transposed = new Matrix(m, n);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transposed.setElement(i, j, this.matrix[i][n-j-1]);
+            }
+        }
+
+        return transposed;
+    }
+
+    public Matrix transposeByHorizontal() {
+        int m = this.numOfRows;
+        int n = this.numOfColumns;
+
+        double[][] array = new double[m][n];
+        for (int i = 0; i < m; i++) {
+            array[i] = this.matrix[m-i-1];
+        }
+
+        Matrix transposed = new Matrix(m, n);
+        transposed.setMatrix(array);
+
+        return transposed;
+    }
+
+    public Double findDeterminant() {
+
+        if (this.numOfRows != this.numOfColumns) {
+            return null;
+        }
+
+        if (this.numOfRows == 2) {
+            double determinant = this.matrix[0][0] * this.matrix[1][1] - this.matrix[0][1] * this.matrix[1][0];
+            return determinant;
+        }
+
+        double determinant = 0;
+        int factor;
+        int n = this.getNumOfRows();
+        Matrix submatrix;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                factor = 1;
+            } else {
+                factor = -1;
+            }
+
+            submatrix = this.findSubmatrix(0, i);
+            determinant +=  this.matrix[0][i] * factor * submatrix.findDeterminant();
+        }
+
+        return determinant;
+    }
+
+    public Matrix findInverse() {
+        int n = this.numOfRows;
+        Matrix submatrix;
+        Matrix cofactors = new Matrix(n, n);
+        int factor;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i + j) % 2 == 0){
+                    factor = 1;
+                } else {
+                    factor = -1;
+                }
+                submatrix = this.findSubmatrix(i, j);
+                cofactors.setElement(i, j, factor * submatrix.findDeterminant());
+            }
+        }
+
+        Matrix transposedCofactors = cofactors.transposeByMain();
+        double oneOverDet = 1 / this.findDeterminant();
+        Matrix inverse = transposedCofactors.multiplyBy(oneOverDet);
+
+        return inverse;
+    }
+
+    public double round(double value, int places) {
+        BigDecimal decimal = BigDecimal.valueOf(value);
+        decimal = decimal.setScale(places, RoundingMode.HALF_UP);
+        return decimal.doubleValue();
+    }
+
+    public Matrix findSubmatrix(int row, int column) {
+        int n = numOfRows;
+        Matrix submatrix = new Matrix(n-1, n-1);
+        int columnIndex = 0;
+        int rowIndex = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (i == row) {
+                rowIndex++;
+            }
+
+            columnIndex = 0;
+            for (int j = 0; j < n - 1; j++) {
+                if (j == column) {
+                    columnIndex++;
+                }
+                submatrix.setElement(i, j, this.matrix[rowIndex][columnIndex]);
+                columnIndex++;
+            }
+            rowIndex++;
+        }
+
+        return submatrix;
+    }
+
 
     @Override
     public boolean equals(Object o) {
@@ -78,10 +252,11 @@ public class Matrix {
     @Override
     public String toString() {
         StringBuilder output = new StringBuilder();
-
+        String spaces;
         for (int i = 0; i < this.numOfRows; i++) {
             for (int j = 0; j < this.numOfColumns; j++) {
-                output.append(this.matrix[i][j]).append(" ");
+//                spaces = new String(new char[generation]).replace("\0", "-");
+                output.append(this.round(this.matrix[i][j], 2)).append(" ");
             }
             output.append("\n");
         }
--- Numeric Matrix Processor/task/src/processor/TextUserInterface.java
+++ Numeric Matrix Processor/task/src/processor/TextUserInterface.java
@@ -1,6 +1,5 @@
 package processor;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TextUserInterface {
@@ -11,23 +10,94 @@ public class TextUserInterface {
     }
 
     public void start() {
-        getSum();
+        String selectedOption;
+        do {
+            printMenu();
+            selectedOption = scanner.nextLine();
+            if (selectedOption.equals("0")) {
+                break;
+            } else if (selectedOption.isBlank()) {
+                continue;
+            }
+
+            handleSelection(selectedOption);
+
+        } while (!selectedOption.equals("0"));
+    }
+
+    public void printMenu() {
+        System.out.println("\n1. Add matrices");
+        System.out.println("2. Multiply matrix to a constant");
+        System.out.println("3. Multiply matrices");
+        System.out.println("4. Transpose matrix");
+        System.out.println("5. Calculate a determinant");
+        System.out.println("6. Inverse matrix");
+        System.out.println("0. Exit");
+        System.out.print("Your choice: ");
+    }
+
+    public void handleSelection(String selectedOption) {
+        int option = Integer.valueOf(selectedOption);
+        switch (option) {
+            case 1:
+                getSum();
+                break;
+            case 2:
+                getScalarProduct();
+                break;
+            case 3:
+                getDotProduct();
+                break;
+            case 4:
+                getTranspose();
+                break;
+            case 5:
+                getDeterminant();
+                break;
+            case 6:
+                getInverse();
+                break;
+        }
+    }
+
+    public void getScalarProduct() {
+        Matrix matrix = initializeMatrix("the");
+        setMatrixFromInput(matrix);
+
+        System.out.print("Enter the multiplier: ");
+        String input = scanner.nextLine();
+        int factor = Integer.valueOf(input);
+        System.out.println("The multiplication result is:");
+        System.out.println(matrix.multiplyBy(factor));
+    }
+
+    public void getDotProduct() {
+        Matrix matrixOne = initializeMatrix("first");
+        setMatrixFromInput(
+                matrixOne);
+
+        Matrix matrixTwo = initializeMatrix("second");
+        setMatrixFromInput(
+                matrixTwo);
+
+        Matrix product = matrixOne.multiplyBy(matrixTwo);
+        if (product != null) {
+            System.out.println("The multiplication result is:");
+            System.out.println(product);
+        } else {
+            System.out.println("ERROR");
+        }
     }
 
     public void getSum() {
 //        System.out.println("Input:\n");
+        Matrix matrixOne = initializeMatrix("first");
+        setMatrixFromInput(
+                matrixOne);
 
-        String[] dimensions = scanner.nextLine().split(" ");
-        int m = Integer.valueOf(dimensions[0]);
-        int n = Integer.valueOf(dimensions[1]);
-        Matrix matrixOne = new Matrix(m, n);
-        setMatrixFromInput(m, n, matrixOne);
-
-        dimensions = scanner.nextLine().split(" ");
-        m = Integer.valueOf(dimensions[0]);
-        n = Integer.valueOf(dimensions[1]);
-        Matrix matrixTwo = new Matrix(m, n);
-        setMatrixFromInput(m, n, matrixTwo);
+        Matrix matrixTwo = initializeMatrix("second");
+        setMatrixFromInput(
+                matrixTwo);
 
 //        System.out.println("\nOutput:\n");
         Matrix sum = matrixOne.add(matrixTwo);
@@ -38,15 +108,73 @@ public class TextUserInterface {
         }
     }
 
-    public void setMatrixFromInput(int m, int n, Matrix matrix) {
+    public void getTranspose() {
+        System.out.println("\n1. Main diagonal");
+        System.out.println("2. Side diagonal");
+        System.out.println("3. Vertical line");
+        System.out.println("4. Horizontal line");
+        System.out.print("Your choice: ");
 
+        String selectedOption = this.scanner.nextLine();
+        Matrix matrix = initializeMatrix("the");
+        setMatrixFromInput(matrix);
+
+        Matrix transposed = null;
+        int option = Integer.valueOf(selectedOption);
+        switch (option) {
+            case 1:
+                transposed = matrix.transposeByMain();
+                break;
+            case 2:
+                transposed = matrix.transposeBySide();
+                break;
+            case 3:
+                transposed = matrix.transposeByVertical();
+                break;
+            case 4:
+                transposed = matrix.transposeByHorizontal();
+        }
+
+        System.out.println("The result is:");
+        System.out.println(transposed);
+    }
+
+    public void getDeterminant() {
+        Matrix matrix = initializeMatrix("the");
+        setMatrixFromInput(matrix);
+
+        System.out.println("The result is:");
+        System.out.println(matrix.findDeterminant());
+    }
+
+    public void getInverse() {
+        Matrix matrix = initializeMatrix("the");
+        setMatrixFromInput(matrix);
+
+        System.out.println("The result is:");
+        System.out.println(matrix.findInverse());
+    }
+
+    public Matrix initializeMatrix(String firstOrSecond) {
+        System.out.print("Enter size of " + firstOrSecond + " matrix:");
+        String[] dimensions = scanner.nextLine().split(" ");
+        int m = Integer.valueOf(dimensions[0]);
+        int n = Integer.valueOf(dimensions[1]);
+
+        System.out.println("Enter " + firstOrSecond + " matrix:");
+        return new Matrix(m, n);
+    }
+
+    public void setMatrixFromInput(Matrix matrix) {
+        int m = matrix.getNumOfRows();
+        int n = matrix.getNumOfColumns();
         String input;
-        String[] numbers = new String[n];
+        String[] numbers;
         for (int i = 0; i < m; i++) {
             input = scanner.nextLine();
             numbers = input.split(" ");
             for (int j = 0; j < n; j++) {
-                matrix.setElement(i, j, Integer.valueOf(numbers[j]));
+                matrix.setElement(i, j, Double.valueOf(numbers[j]));
             }
         }
     }
